d4f19fcc5d5164bb8a10b0bd0d83bbc721b391fb
Scott Stamp <scottstamp851@gmail.com>
2024-06-30 01:55:13 -0230

    testing

--- README.md
+++ README.md
@@ -1,24 +1,15 @@
-# TalkToShout
+# ModelSwap
 **Made for Habbo: Origins ONLY**
 
-Converts talking packets to shouting packets and the otherway around using [GoEarth](https://github.com/xabbo/goearth)
+Uses [GoEarth](https://github.com/xabbo/goearth)
 
 ## Features
-- Converts Outgoing CHAT packet to SHOUT
-- Converts Outgoing SHOUT packet to CHAT
-- Dance from chat using `/dance`
-- Wave by saying `o/` (anywhere in the text)
-- Hold shift to talk
 
 ### Known Issues
-- none
-
-### why?
-Switching rooms makes you always select Say instead of Shout by default and I want to switch the functionality of these 2 so that talk will be shout and shout will be talk, 
 
 ### Buildcommand used:
 `go build -ldflags -H=windowsgui`
 
 ### Credits:
-- me for this
+- [Edaurd] for [TalkToShout](https://github.com/Edaurd/TalkToShout) which I used as an example because I'm lazy
 - [b7](https://github.com/b7c) for [GoEarth](https://github.com/xabbo/goearth)
--- main.go
+++ main.go
@@ -5,52 +5,60 @@ import (
 
 	g "xabbo.b7c.io/goearth"
 	"xabbo.b7c.io/goearth/shockwave/out"
+	"xabbo.b7c.io/goearth/shockwave/in"
 )
 
 var ext = g.NewExt(g.ExtInfo{
-	Title:       "Talk To Shout",
-	Description: "Shout To Talk, reverses the functionality for ease of use :)",
+	Title:       "ModelSwap",
+	Description: "",
 	Version:     "1.0",
-	Author:      "Eduard, b7",
+	Author:      "Scott",
 })
 
 var emoteBlock bool
 
 func main() {
-	ext.Intercept(out.CHAT).With(handleTalk)
-	ext.Intercept(out.SHOUT).With(handleShout)
+	// ext.Intercept(out.CHAT).With(handleTalk)
+	ext.Intercept(in.ROOM_READY).With(handleRoomReady)
 	ext.Run()
 }
 
-func handleShout(e *g.InterceptArgs) {
-	msg := e.Packet.ReadString()
-	handleEmotes(msg)
+func handleRoomReady(e *g.InterceptArgs) {
+	roomInfo := e.Packet.ReadString()
+	modifiedRoomInfo := strings.replace(roomInfo, "model_z", "model_f", 1)
 	e.Block()
-	if !emoteBlock {
-		ext.Send(out.CHAT, msg)
-	}
-}
-func handleTalk(e *g.InterceptArgs) {
-	msg := e.Packet.ReadString()
-	handleEmotes(msg)
-	e.Block()
-	if !emoteBlock {
-		ext.Send(out.SHOUT, msg)
-	}
+	ext.Send(in.ROOM_READY, modifiedRoomInfo)
 }
 
-func handleEmotes(msg string) {
-	if msg == "/dance" {
-		emoteBlock = true
-		ext.Send(out.DANCE)
-	} else if strings.Contains(msg, "o/") {
-		if msg == "o/" {
-			emoteBlock = true
-		} else {
-			emoteBlock = false
-		}
-		ext.Send(out.WAVE)
-	} else {
-		emoteBlock = false
-	}
-}
+// func handleShout(e *g.InterceptArgs) {
+// 	msg := e.Packet.ReadString()
+// 	handleEmotes(msg)
+// 	e.Block()
+// 	if !emoteBlock {
+// 		ext.Send(out.CHAT, msg)
+// 	}
+// }
+// func handleTalk(e *g.InterceptArgs) {
+// 	msg := e.Packet.ReadString()
+// 	handleEmotes(msg)
+// 	e.Block()
+// 	if !emoteBlock {
+// 		ext.Send(out.SHOUT, msg)
+// 	}
+// }
+
+// func handleEmotes(msg string) {
+// 	if msg == "/dance" {
+// 		emoteBlock = true
+// 		ext.Send(out.DANCE)
+// 	} else if strings.Contains(msg, "o/") {
+// 		if msg == "o/" {
+// 			emoteBlock = true
+// 		} else {
+// 			emoteBlock = false
+// 		}
+// 		ext.Send(out.WAVE)
+// 	} else {
+// 		emoteBlock = false
+// 	}
+// }
